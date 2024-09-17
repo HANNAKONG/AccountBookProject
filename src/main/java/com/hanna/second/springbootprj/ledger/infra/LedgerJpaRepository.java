077b4b75b22c8de14521d280d5b24390575a8285
khn1346 <khn1346@gmail.com>
2024-09-18 01:51:13 +0900

    fix: LedgerRepository 상속 X, import 부분 정리

--- src/main/java/com/hanna/second/springbootprj/ledger/infra/LedgerJpaRepository.java
+++ src/main/java/com/hanna/second/springbootprj/ledger/infra/LedgerJpaRepository.java
@@ -1,12 +1,11 @@
 package com.hanna.second.springbootprj.ledger.infra;
 
 import com.hanna.second.springbootprj.ledger.domain.Ledger;
-import com.hanna.second.springbootprj.ledger.domain.LedgerRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface LedgerJpaRepository extends JpaRepository<Ledger, Long>, LedgerRepository {
+public interface LedgerJpaRepository extends JpaRepository<Ledger, Long> {
     @Override
     void delete(Ledger entity);
 
