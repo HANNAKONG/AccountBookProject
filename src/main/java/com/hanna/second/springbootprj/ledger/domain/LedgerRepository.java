2a4f0132dfc19e9c0818c47b018e9aff8ff70008
khn1346 <khn1346@gmail.com>
2024-09-18 01:51:35 +0900

    fix: findAllByFilter 추가

--- src/main/java/com/hanna/second/springbootprj/ledger/domain/LedgerRepository.java
+++ src/main/java/com/hanna/second/springbootprj/ledger/domain/LedgerRepository.java
@@ -1,11 +1,13 @@
 package com.hanna.second.springbootprj.ledger.domain;
 
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import com.hanna.second.springbootprj.ledger.dto.LedgerRequestDto;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface LedgerRepository extends JpaSpecificationExecutor<Ledger> {
+public interface LedgerRepository {
     void delete(Ledger ledger);
     Ledger save(Ledger ledger);
     Optional<Ledger> findById(Long Id);
+    List<Ledger> findAllByFilter(LedgerRequestDto ledgerRequestDto);
 }
