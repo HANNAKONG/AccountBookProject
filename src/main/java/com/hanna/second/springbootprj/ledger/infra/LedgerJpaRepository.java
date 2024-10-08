9a3f23117fc945f5ea8efa9e9901ff055eead44c
khn1346 <khn1346@gmail.com>
2024-10-08 19:52:49 +0900

    feat: ledger list 조회 -> 수정 중

--- src/main/java/com/hanna/second/springbootprj/ledger/infra/LedgerJpaRepository.java
+++ src/main/java/com/hanna/second/springbootprj/ledger/infra/LedgerJpaRepository.java
@@ -1,18 +1,9 @@
 package com.hanna.second.springbootprj.ledger.infra;
 
 import com.hanna.second.springbootprj.ledger.domain.Ledger;
+import com.hanna.second.springbootprj.ledger.domain.LedgerRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface LedgerJpaRepository extends JpaRepository<Ledger, Long> {
-    @Override
-    void delete(Ledger entity);
-
-    @Override
-    Ledger save(Ledger entity);
-
-    @Override
-    Optional<Ledger> findById(Long Id);
+public interface LedgerJpaRepository extends JpaRepository<Ledger, Long>, LedgerRepositoryCustom {
 
 }
