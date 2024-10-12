6b096510c09c68f13b136109a8f3d0524bf6910f
khn1346 <khn1346@gmail.com>
2024-10-13 01:27:56 +0900

    add: 카테고리 추가

--- src/main/java/com/hanna/second/springbootprj/support/enums/CategoryType.java
+++ src/main/java/com/hanna/second/springbootprj/support/enums/CategoryType.java
@@ -1,7 +1,11 @@
 package com.hanna.second.springbootprj.support.enums;
 
 public enum CategoryType {
-    FOOD;
+    FOOD,
+    TRAVEL,
+    ENTERTAINMENT,
+    SHOPPING,
+    UNDECIDED;
 
     public String getCategoryType() {
         return name();
