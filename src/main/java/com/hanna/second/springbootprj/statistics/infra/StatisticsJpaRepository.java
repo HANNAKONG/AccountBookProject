b0d3b60099483a1b02d9edb45df103d4e43a57db
khn1346 <khn1346@gmail.com>
2024-10-13 18:27:08 +0900

    fix: 메서드 정리

--- src/main/java/com/hanna/second/springbootprj/statistics/infra/StatisticsJpaRepository.java
+++ src/main/java/com/hanna/second/springbootprj/statistics/infra/StatisticsJpaRepository.java
@@ -1,20 +1,12 @@
 package com.hanna.second.springbootprj.statistics.infra;
 
 import com.hanna.second.springbootprj.statistics.domain.Statistics;
+import com.hanna.second.springbootprj.statistics.domain.StatisticsRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface StatisticsJpaRepository extends JpaRepository<Statistics, Long> {
-    @Override
-    void delete(Statistics entity);
-
-    @Override
-    Statistics save(Statistics entity);
-
-    @Override
-    Optional<Statistics> findById(Long Id);
-
+public interface StatisticsJpaRepository extends JpaRepository<Statistics, Long>, StatisticsRepositoryCustom {
     Optional<Statistics> findByBaseDateAndUsersId(String baseDate, Long usersId);
 }
 
